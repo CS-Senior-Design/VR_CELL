4b9731730f052121fc723021d26d774c7d51341f
Derrick Keough <derrickkeough@yahoo.com>
2022-07-14 19:55:52 -0400

    support true/false questions, quiz scripting WIP

--- VR_Cell/Assets/Scripts/Derrick/Quiz UI/QuizManager.cs
+++ VR_Cell/Assets/Scripts/Derrick/Quiz UI/QuizManager.cs
@@ -1,27 +1,60 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
-using TMPro;
 
 public class QuizManager : MonoBehaviour
 {
     public TMP_Text questionNumber;
-    public TMP_Text questionPrompt;
-    public TMP_Text promptA, promptB, promptC, promptD;
 
-    private Stack<Question> questionStack, questionStackBin;
+    public TMP_Text questionPrompt;
+
+    public TMP_Text
+
+            promptA,
+            promptB,
+            promptC,
+            promptD;
+
+    public GameObject
+
+            buttonA,
+            buttonB,
+            buttonC,
+            buttonD;
+
+    private Stack<Question>
+
+            questionStack,
+            questionStackBin;
 
     private int questionCounter;
+
     private int numOfQuestions;
+
     private Question currentQuestion;
-    private string prompt;
-    private string A, B, C, D;
 
     void Start()
     {
         questionStack = new Stack<Question>();
         questionStackBin = new Stack<Question>();
+    }
+
+    public void checkAnswer()
+    {
+    }
+
+    public void turnButtonRed(GameObject _button)
+    {
+    }
+
+    public void turnButtonGreen(GameObject _button)
+    {
+    }
+
+    public void turnButtonWhite(GameObject _button)
+    {
     }
 
     public void StartQuiz(Question[] questions)
@@ -33,13 +66,13 @@ public class QuizManager : MonoBehaviour
 
         foreach (Question q in questions)
         {
-            questionStackBin.Push(q);
+            questionStackBin.Push (q);
         }
 
         foreach (Question q in questions)
         {
             Question tempQuestion = questionStackBin.Pop();
-            questionStack.Push(tempQuestion);
+            questionStack.Push (tempQuestion);
             Debug.Log(tempQuestion.questionPrompt);
         }
 
@@ -48,24 +81,49 @@ public class QuizManager : MonoBehaviour
 
     public void DisplayNextQuestion()
     {
+        // On each new question, reset all button colors back to gray
+        // turnButtonWhite(buttonA);
+        // turnButtonWhite(buttonB);
+        // turnButtonWhite(buttonC);
+        // turnButtonWhite(buttonD);
         if (questionStack.Count == 0)
         {
             EndQuiz();
             return;
         }
 
-        // Insert logic here.
-        questionStackBin.Push(currentQuestion);
+        questionStackBin.Push (currentQuestion);
         currentQuestion = questionStack.Pop();
 
-        questionCounter++;
-        questionNumber.text = "Question " + questionCounter.ToString() + "/" + numOfQuestions;
+        // If question is true/false, de-activate two answer choices
+        if (currentQuestion.questionOptions.Length == 2)
+        {
+            buttonA.SetActive(false);
+            buttonC.SetActive(false);
 
-        questionPrompt.text = currentQuestion.questionPrompt;
-        promptA.text = currentQuestion.questionOptions[0];
-        promptB.text = currentQuestion.questionOptions[1];
-        promptC.text = currentQuestion.questionOptions[2];
-        promptD.text = currentQuestion.questionOptions[3];
+            questionCounter++;
+            questionNumber.text =
+                "Question " + questionCounter.ToString() + "/" + numOfQuestions;
+
+            questionPrompt.text = currentQuestion.questionPrompt;
+            promptB.text = currentQuestion.questionOptions[0];
+            promptD.text = currentQuestion.questionOptions[1];
+        }
+        else
+        // Otherwise question is standard 4-multiple-choice; activate the answer choices
+        {
+            buttonA.SetActive(true);
+            buttonC.SetActive(true);
+            questionCounter++;
+            questionNumber.text =
+                "Question " + questionCounter.ToString() + "/" + numOfQuestions;
+
+            questionPrompt.text = currentQuestion.questionPrompt;
+            promptA.text = currentQuestion.questionOptions[0];
+            promptB.text = currentQuestion.questionOptions[1];
+            promptC.text = currentQuestion.questionOptions[2];
+            promptD.text = currentQuestion.questionOptions[3];
+        }
     }
 
     public void DisplayPreviousQuestion()
@@ -76,18 +134,37 @@ public class QuizManager : MonoBehaviour
             return;
         }
 
-        // Insert logic here.
-        questionStack.Push(currentQuestion);
+        questionStack.Push (currentQuestion);
         currentQuestion = questionStackBin.Pop();
 
-        questionCounter--;
-        questionNumber.text = "Question " + questionCounter.ToString() + "/" + numOfQuestions;
+        // If question is true/false, de-activate two answer choices
+        if (currentQuestion.questionOptions.Length == 2)
+        {
+            buttonA.SetActive(false);
+            buttonC.SetActive(false);
+            questionCounter--;
+            questionNumber.text =
+                "Question " + questionCounter.ToString() + "/" + numOfQuestions;
 
-        questionPrompt.text = currentQuestion.questionPrompt;
-        promptA.text = currentQuestion.questionOptions[0];
-        promptB.text = currentQuestion.questionOptions[1];
-        promptC.text = currentQuestion.questionOptions[2];
-        promptD.text = currentQuestion.questionOptions[3];
+            questionPrompt.text = currentQuestion.questionPrompt;
+            promptB.text = currentQuestion.questionOptions[0];
+            promptD.text = currentQuestion.questionOptions[1];
+        }
+        else
+        // Otherwise question is standard 4-multiple-choice; activate the answer choices
+        {
+            buttonA.SetActive(true);
+            buttonC.SetActive(true);
+            questionCounter--;
+            questionNumber.text =
+                "Question " + questionCounter.ToString() + "/" + numOfQuestions;
+
+            questionPrompt.text = currentQuestion.questionPrompt;
+            promptA.text = currentQuestion.questionOptions[0];
+            promptB.text = currentQuestion.questionOptions[1];
+            promptC.text = currentQuestion.questionOptions[2];
+            promptD.text = currentQuestion.questionOptions[3];
+        }
     }
 
     void EndQuiz()
