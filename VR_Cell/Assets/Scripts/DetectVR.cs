d47a5c87b180da6c786730041927dace0a92fd3c
Michael <michael.nehgrei@gmail.com>
2022-07-14 17:39:21 -0400

    checkbox for starting in VR

--- VR_Cell/Assets/Scripts/DetectVR.cs
+++ VR_Cell/Assets/Scripts/DetectVR.cs
@@ -3,37 +3,44 @@ using UnityEngine.XR.Management;
 
 public class DetectVR : MonoBehaviour
 {
+    public bool startInVR = true;
     public GameObject xrOrigin;
     public GameObject desktopPlayer;
     
     // Start is called before the first frame update
     void Start()
     {
-        var xrSettings = XRGeneralSettings.Instance;
-        if(xrSettings == null) {
-            Debug.Log("XRGeneralSettings is null");
-            return;
-        }
+        if(startInVR) {
 
-        var xrManager = xrSettings.Manager;
-        if(xrSettings == null) {
-            Debug.Log("XRManagerSettings is null");
-            return;
-        }
+            var xrSettings = XRGeneralSettings.Instance;
+            if(xrSettings == null) {
+                Debug.Log("XRGeneralSettings is null");
+                return;
+            }
 
-        var xrLoader = xrManager.activeLoader;
-        if(xrSettings == null) {
-            Debug.Log("XRLoader is null");
+            var xrManager = xrSettings.Manager;
+            if(xrSettings == null) {
+                Debug.Log("XRManagerSettings is null");
+                return;
+            }
+
+            var xrLoader = xrManager.activeLoader;
+            if(xrSettings == null) {
+                Debug.Log("XRLoader is null");
+                xrOrigin.SetActive(false);
+                desktopPlayer.SetActive(true);
+                return;
+            }
+
+            Debug.Log("XRLoader is NOT null");
+            xrOrigin.SetActive(true);
+            desktopPlayer.SetActive(false);
+            return;
+
+        } else {
             xrOrigin.SetActive(false);
             desktopPlayer.SetActive(true);
-            return;
         }
-
-        Debug.Log("XRLoader is NOT null");
-        xrOrigin.SetActive(true);
-        desktopPlayer.SetActive(false);
-        return;
-
 
     }
 }
