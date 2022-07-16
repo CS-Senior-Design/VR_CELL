234388b05e0dcd7ba362d587a5ef139e61f5e3a9
santy81855 <mynameissantiagogarcia@gmail.com>
2022-07-16 18:54:35 -0400

    fixed teleport script

--- VR_Cell/Assets/Scenes/Sara/Scripts/TeleportationManager.cs
+++ VR_Cell/Assets/Scenes/Sara/Scripts/TeleportationManager.cs
@@ -15,11 +15,16 @@ public class TeleportationManager : MonoBehaviour
     [SerializeField] private InteractionLayerMask teleportationLayers;
     [SerializeField] private TeleportationProvider provider;
     
-    private bool _isActive;
+    // whether the teleportation ray is active or not
+    private bool _isActive = false;
+    // detects whether the activate button is pressed
+    private bool _isPressed = false; 
+    // if the ray is on an invalid object
+    private bool _isValidTarget = true;
     private InteractionLayerMask initialInteractionLayers;
     private List<IXRInteractable> interactables = new List<IXRInteractable>();
 
-    private bool isPressed = false; 
+    
 
     // Start is called before the first frame update
     void Start()
@@ -42,60 +47,74 @@ public class TeleportationManager : MonoBehaviour
     // // Update is called once per frame
     void Update()
     {
-        if (!_isActive)
+        if (_isActive == false)
             return;
         
         if (activate.action.triggered)
         {
-            Debug.Log("activate triggered");
             return;
         }
 
         rayInteractor.GetValidTargets(interactables);
 
+        // if the ray is pointing at non-interactable objects
         if (interactables.Count == 0)
         {
-            TurnOffTeleport();
+            // set _isValidTarget to false
+            _isValidTarget = false;
             return;
         }
+        // if the ray is pointing at interactable objects then set it to true
+        else 
+            _isValidTarget = true;
 
         rayInteractor.TryGetCurrent3DRaycastHit(out RaycastHit hit);
 
         TeleportRequest request = new TeleportRequest();
 
-        // for teleportation area
+        // if pointing at a teleportation area
         if (interactables[0].interactionLayers == 4)
         {
-            Debug.Log("Teleportation Area");
             request.destinationPosition = hit.point;
         }
-        // for teleporation anchor
+        // if pointing at a teleporation anchor
         else if (interactables[0].interactionLayers == 16)
         {
-            Debug.Log("Teleportation Anchor");
             request.destinationPosition = hit.transform.GetChild(0).transform.position;
         }
-        if (isPressed == false)
+        // if the ray is on an invalid object
+        else
+        {
+            _isValidTarget = false;
+        }
+        // if the activate button is pressed while in teleport mode it means we want to teleport
+        if (_isPressed == false)
         {
             provider.QueueTeleportRequest(request);
             TurnOffTeleport();
         }
-
     }
 
     private void OnTeleportActivate(InputAction.CallbackContext context)
     {
-        // if (gripModeActivate.action.phase != InputActionPhase.Performed)
-        // {
-            if (isPressed == false)
-                isPressed = true;
+        // if the currently pointed at area is not a valid teleport target, we don't want to teleport
+        if (_isValidTarget == false)
+        {
+            return;
+        }
+        // if it is a valid target then we want to initiate teleport stuff
+        else
+        {
+            if (_isPressed == false)
+                _isPressed = true;
             else
-                isPressed = false;
+                _isPressed = false;
+            // toggle _isActive to true
             _isActive = true;
+            // change the ray interactor to the teleport settings
             rayInteractor.lineType = XRRayInteractor.LineType.ProjectileCurve;
             rayInteractor.interactionLayers = teleportationLayers;
-        // }
-
+        }
     }
 
     private void OnTeleportCancel(InputAction.CallbackContext context)
@@ -105,10 +124,9 @@ public class TeleportationManager : MonoBehaviour
 
     private void TurnOffTeleport()
     {
-        if (isPressed == false)
-            isPressed = true;
-        else
-            isPressed = false;
+        // the active button was "released"
+        _isPressed = false;
+        // the teleportation ray should be turned off
         _isActive = false;
         rayInteractor.lineType = XRRayInteractor.LineType.StraightLine;
         rayInteractor.interactionLayers = initialInteractionLayers;
