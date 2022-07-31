70efeaaf00c0f574e618a349ef28e3a7eb3a4983
santy <mynameissantiagogarcia@gmail.com>
2022-07-31 12:19:21 -0400

    changed ribosome generation to generate subunits and create full ribosome upon combining them

--- VR_Cell/Assets/Scripts/ImmersiveScripts/GenerateRibosomes.cs
+++ VR_Cell/Assets/Scripts/ImmersiveScripts/GenerateRibosomes.cs
@@ -6,8 +6,9 @@ public class GenerateRibosomes : MonoBehaviour
 {
     // where should the ribosome spawn
     private Vector3 _startPosition = new Vector3(0.0f, 0.0f, 0.0f);
-    // take the ribosome prefab 
-    public GameObject _ribosome;
+    // take the ribosome prefabs 
+    public GameObject _ribosome1;
+    public GameObject _ribosome2;
     // animation speed 
     private float _animationSpeed = 0.07f;
     // gameobject list for the proteins
@@ -20,6 +21,8 @@ public class GenerateRibosomes : MonoBehaviour
     private int _ribosomeCount = 0;
     // variable to change the max number of ribosomes
     private int _maxRibosomes = 100;
+    // variable to track which ribosome to spawn
+    private bool _ribosomeType = true;
 
     // Start is called before the first frame update
     void Start()
@@ -60,8 +63,20 @@ public class GenerateRibosomes : MonoBehaviour
             {
                 index = _proteins.Count - 1;
             }
+            GameObject ribosome = null;
             // spawn a ribosome
-            GameObject ribosome = Instantiate(_ribosome, _startPosition, Quaternion.identity);
+            if (_ribosomeType == true)    
+            {
+                _ribosomeType = false;
+                ribosome = Instantiate(_ribosome1, _startPosition, Quaternion.identity);
+            }
+            else
+            {
+                _ribosomeType = true;
+                ribosome = Instantiate(_ribosome2, _startPosition, Quaternion.identity);
+            }     
+            // make it face towards its target protein
+            ribosome.transform.LookAt(_proteins[index].transform.position, Vector3.right);
             _ribosomeCount++;
             if (_ribosomeCount == _maxRibosomes)
             {
