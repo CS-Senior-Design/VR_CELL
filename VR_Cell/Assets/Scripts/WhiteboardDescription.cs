0fad97ea8b4a7882f6d00dd3ef632b8f96d6b994
santy81855 <mynameissantiagogarcia@gmail.com>
2022-08-01 17:18:13 -0400

    on hover cell description script

--- VR_Cell/Assets/Scripts/WhiteboardDescription.cs
+++ VR_Cell/Assets/Scripts/WhiteboardDescription.cs
@@ -19,7 +19,6 @@ public class WhiteboardDescription : MonoBehaviour
         _description.Add("Smooth ER", "Smooth ER is a component of the endomembrane system. It's involved in the synthesis of lipids and steroids, which are then sent out to be packaged by the Golgi Apparatus.");
         _description.Add("Mitochondria", "Mitochondria generate most of the chemical energy needed to power the cell's biochemical reactions.");
         _description.Add("Golgi Apparatus", "The Golgi Apparatus acts as a factory in which proteins received from the rough ER are further processed and sorted for transport to their eventual destinations: lysosomes, the plasma membrane, or secretion.");
-        _description.Add("Nuclear Membrane", "The function of the nuclear membrane is to act as a barrier that separates the contents of the nucleus from the cytoplasm.");
         _description.Add("Centrosomes", "A centrosome is a cellular structure involved in the process of cell division.");
     }
 
