dfc74d2accd7baa3ed12bddc0e2c6bde67af3f06
santy81855 <mynameissantiagogarcia@gmail.com>
2022-08-01 18:51:28 -0400

    lab board final menu

--- VR_Cell/Assets/Scripts/MenuScripts/MenuControl.cs
+++ VR_Cell/Assets/Scripts/MenuScripts/MenuControl.cs
@@ -1,33 +1,48 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class MenuControl : MonoBehaviour
 {
     public GameObject _immersiveText;
     public GameObject _labText;
+    public GameObject _labBoard;
     public GameObject _quitText;
     public GameObject _cellMembrane;
     public GameObject _cell;
 
     private Color _cellMembraneMaterial;
 
+    public GameObject _boardText;
+    public GameObject _boardTitleText;
+
+    private Color _boardMaterial;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        //get original cell membrane material
+        _cellMembraneMaterial = _cellMembrane.GetComponent<Renderer>().material.color;
+        //get original board material
+        _boardMaterial = _labBoard.GetComponent<Renderer>().material.color;
     }
     
     // function to call on cell hover
     public void cellHover()
     {
+        //change the board title text
+        _boardTitleText.GetComponent<TMPro.TextMeshProUGUI>().text = "Immersive Mode";
+        //Change the board text 
+        _boardText.GetComponent<TMPro.TextMeshProUGUI>().text = "Jump inside the cell and explore!";
+
+
+
         // scale the cell up a little
         _cell.transform.localScale = new Vector3(1.1f, 1.1f, 1.1f);
 
         // highlight the cell a little
-        _cellMembraneMaterial = _cellMembrane.GetComponent<Renderer>().material.color;
-        _cellMembrane.GetComponent<Renderer>().material.color = new Color(224f, 215f, 61f);
+        _cellMembrane.GetComponent<Renderer>().material.color = new Color(255,255,0);
 
         // immersive title shows up
         _immersiveText.SetActive(true);
@@ -35,6 +50,10 @@ public class MenuControl : MonoBehaviour
 
     public void cellHoverExit()
     {
+        _boardTitleText.GetComponent<TMPro.TextMeshProUGUI>().text = "Pick a mode!";
+        //Change the board text 
+        _boardText.GetComponent<TMPro.TextMeshProUGUI>().text = "Choose between Immersive Mode and Lab Mode.";
+
         // scale the cell down a little
         _cell.transform.localScale = new Vector3(1f, 1f, 1f);
 
@@ -44,4 +63,32 @@ public class MenuControl : MonoBehaviour
         // immersive title disappears
         _immersiveText.SetActive(false);
     }
+
+    public void boardHover()
+    {
+        //change the board title text
+        _boardTitleText.GetComponent<TMPro.TextMeshProUGUI>().text = "Lab Mode";
+        //Change the board text
+        _boardText.GetComponent<TMPro.TextMeshProUGUI>().text = "Explore the lab and learn about the cell!";
+        //scale the board up a little
+        _labBoard.transform.localScale = new Vector3(1.1f, 1.1f, 1.1f);
+        //highlight the board a little
+        _labBoard.GetComponent<Renderer>().material.color = new Color(255,255,0);
+        //lab title shows up
+        _labText.SetActive(true);
+    }
+
+    public void boardHoverExit()
+    {
+        _boardTitleText.GetComponent<TMPro.TextMeshProUGUI>().text = "Pick a mode!";
+        //Change the board text 
+        _boardText.GetComponent<TMPro.TextMeshProUGUI>().text = "Choose between Immersive Mode and Lab Mode.";
+        //scale the board down a little
+        _labBoard.transform.localScale = new Vector3(1f, 1f, 1f);
+        //unhighlight the board
+        _labBoard.GetComponent<Renderer>().material.color = _boardMaterial;
+        //lab title disappears
+        _labText.SetActive(false);
+    }
+
 }
