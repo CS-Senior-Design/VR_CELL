f1d69a1524b5bb853a940a21d0e038ef72415425
Derrick K <60911261+Dpk417182@users.noreply.github.com>
2022-08-01 13:41:40 -0400

    Quiz bug squashing (#8)
    * WIP: Quiz debugging and updating
    
    * Quiz logic finalized, Quiz UI Prefab added to

--- VR_Cell/Assets/Scripts/Derrick/Quiz UI/QuizTrigger.cs
+++ VR_Cell/Assets/Scripts/Derrick/Quiz UI/QuizTrigger.cs
@@ -2,10 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class QuizTrigger : MonoBehaviour {
+public class QuizTrigger : MonoBehaviour
+{
     public Question[] questions;
 
-    public void TriggerQuiz() {
+    public void TriggerQuiz()
+    {
         FindObjectOfType<QuizManager>().StartQuiz(questions);
     }
 }
